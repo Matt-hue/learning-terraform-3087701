e49b56b805026c0c2913829c2dbd32287fdbf78f
Matt-hue <62297549+Matt-hue@users.noreply.github.com>
2024-03-23 20:51:54 +0000

    listeners updated

--- main.tf
+++ main.tf
@@ -70,13 +70,15 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port = 80
-      protocol = "HTTP"
-      target_group_index = 0
-    }
-  ]
+   listeners = {
+      http_tcp_listeners = [
+        {
+          port = 80
+          protocol = "HTTP"
+          target_group_index = 0
+        }
+      ]
+   }
 
   tags = {
     Environment = "dev"
